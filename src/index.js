9cb551d2d340cb7e810083487a2055351a6504bf
Pereselakilya <104826618+PereselakIlya@users.noreply.github.com>
2023-09-18 15:17:00 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix==0){
+    return matrix;
+  }
+  else if(matrix==null){
+    return [];
+  }
+  else if(matrix.length%2==0){
+    matrix[matrix.length%2+1]=matrix[matrix.length%2+1].reverse();
+  }
+  else{
+    matrix[matrix.length%2]=matrix[matrix.length%2].reverse();
+  }
+  return matrix.flat();
 }
